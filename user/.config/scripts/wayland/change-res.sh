d43e1b9a4b6770287eb87f2ecb124491a372cbb7
krypek <krypek@tuta.io>
2023-07-04 20:27:18 +0200

    Fix res dialog
    Former-commit-id: 658cf76452de0e5518456bb997a7f4d59fdeeb13 [formerly bbe3b77358ebf3c9407b16dbcd2f3e0dc764c854]
    Former-commit-id: bec0bf64e5daa68fec3478d0adbce90d0bae7e87

--- user/.config/scripts/wayland/change-res.sh
+++ user/.config/scripts/wayland/change-res.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -a
 MONITORS="$(wlr-randr | grep -E '"*"|Enabled|preferred' | awk '{if ($1 ~ "Enabled") print $2 " "; else if ($1 ~ /[0-9]+x[0-9]+/) print $1 "@"; else print $1 " "}' | tr -d '\n' | tr '@' '\n')"
+echo "$MONITORS"
 MONITOR_COUNT="$(echo "$MONITORS" | wc -l)"
+echo "$MONITOR_COUNT"
 
 if [ "$MONITOR_COUNT" != '1' ]; then
-    OUTPUT="$(echo $MONITORS | fuzzel -d --log-level none --width 25 --lines 5 | awk '{print $1}')"
+    OUTPUT="$(echo "$MONITORS" | fuzzel -d --log-level none --width 25 --lines 5 | awk '{print $1}')"
 else
-    OUTPUT="$(echo $MONITORS | awk '{print $1}')"
+    OUTPUT="$(echo "$MONITORS" | awk '{print $1}')"
 fi
 
 RES_TO_SORT="$(wlr-randr --output HDMI-A-1 | awk '/px/ {print $1 "@" $3 "@" int($3+0.5) }' | tr '\n' ' ')"
@@ -36,8 +38,10 @@ for inp in $RES_TO_SORT; do
     fi
 done
 
-SEL="$(printf "$RES_TO_SEL" | fuzzel -d --log-level none --width 17)"
+printf "$RES_TO_SEL"
+SEL="$(printf "$RES_TO_SEL" | sort --general-numeric-sort --reverse | fuzzel -d --log-level none --width 17)"
 [ $? -eq 1 ] && exit
 RES="$(echo $SEL | awk '{print $1}')"
 REFRESH="$(echo $SEL | awk '{print $4}')"
-wlr-randr --output $OUTPUT --mode "$RES@$REFRESH"
+echo wlr-randr --output $OUTPUT --mode "$RES@$REFRESH"
+# wlr-randr --output $OUTPUT --mode "$RES@$REFRESH"
