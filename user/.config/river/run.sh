0d1f24b788efb16656a7c31108523a03e72bcf0d
krypek <krypek@tuta.io>
2024-03-12 17:24:17 +0100

    Update river config
    Former-commit-id: 8e55fd013a0d7bf7baa2fe334719698af5a88580 [formerly 7338d4cd1f2134518ccba6eeeb3605e1c3ed6940]
    Former-commit-id: fd6630692c68aa92c6283d75aee5e9fcd276f06f

--- user/.config/river/run.sh
+++ user/.config/river/run.sh
@@ -7,19 +7,21 @@ export MOZ_ENABLE_WAYLAND=1
 export GDK_BACKEND=wayland
 
 # launches a session dbus instance
-dbuslaunch="$(which dbus-launch 2>/dev/null)"
-if [ -n "$dbuslaunch" ] && [ -x "$dbuslaunch" ] && [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
-    eval $($dbuslaunch --sh-syntax --exit-with-session)
-fi
-
-dbus-update-activation-environment --all > /dev/null 2>&1
+# dbuslaunch="$(which dbus-launch 2>/dev/null)"
+# if [ -n "$dbuslaunch" ] && [ -x "$dbuslaunch" ] && [ -z "$DBUS_SESSION_BUS_ADDRESS" ]; then
+#     eval $($dbuslaunch --sh-syntax --exit-with-session)
+# fi
+# 
+# dbus-update-activation-environment --all > /dev/null 1>&1
 # /usr/bin/gnome-keyring-daemon --start --components=secrets > /dev/null 2>&1 &
 
+# gentoo-pipewire-launcher &
 # simulate a do-while
 do=true
 while $do || [ -f /tmp/restart_river ]; do
     do=false
     rm -rf /tmp/restart_river > /dev/null 2>&1
+    # dbus-launch --exit-with-session river > ~/.config/river/log.txt 2>&1
     river > ~/.config/river/log.txt 2>&1
     [ -f /tmp/restart_river ] && echo Restarting river...
 done
